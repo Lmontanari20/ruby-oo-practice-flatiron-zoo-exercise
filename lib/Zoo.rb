dfdf1657b1391991d922cfbfbd3197bbfe67f264
Lucas Montanari <Lmonty2011@gmail.com>
2020-11-23 09:36:09 -0800

    implemented the zoo and animal classes with correct association

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,35 @@
 class Zoo
+    attr_accessor :name, :location
 
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def animals
+        Animal.all.select{|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        self.animals.map{|animal| animal.species}.uniq
+    end
+
+    def find_by_species(specie)
+        self.animals.select{|animal| animal.species == specie}
+    end
+
+    def animal_nicknames
+        self.animals.map{|animal| animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        Zoo.all.select{|zoo| zoo.location == location}
+    end
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,24 @@ require 'pry'
 
 
 #Test your code here
+z1 = Zoo.new("my zoo", "Santa Monica")
+z2 = Zoo.new("your zoo", "San Fran")
+z3 = Zoo.new("zoo who", "Santa Monica")
+
+a1 = Animal.new("dog", 80, "Raven", z1)
+a2 = Animal.new("cat", 20, "penelope", z1)
+a3 = Animal.new("dog", 70, "cali", z1)
+a4 = Animal.new("lion", 220, "steve", z2)
+a5 = Animal.new("giraffe", 200, "jon", z2)
+a6 = Animal.new("kangaroo", 120, "mo", z3)
+
+puts a1.zoo
+puts Animal.find_by_species("dog")
+puts z1.animals
+puts z2.animal_species
+puts z2.find_by_species("lion")
+puts z2.animal_nicknames
+puts Zoo.find_by_location("Santa Monica")
 
 
 binding.pry
